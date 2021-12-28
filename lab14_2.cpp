f92dbfacfab9680939a46935cd383ec1d1e68014
Pitipat <pitipat_fu@cmu.ac.th>
2021-12-28 17:46:03 +0700

    commit Lap14_2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,36 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool A[N][M]){
+    
+    cout << "------------------------------------------------------------------------" << "\n";
+    for (int i = 0; i < N ; i++)
+    {
+        cout << "|" ;
+        for (int j = 0; j < M ; j++)
+        {
+            if(A[i][j] == 0) cout << " " ;
+            else if (A[i][j] == 1 ) cout << "*" ;
+        }
+        cout << "|" ;
+        cout << "\n" ; 
+    }
+    cout << "------------------------------------------------------------------------" << "\n" ;
+    
+}
+
+void updateImage(bool B[N][M],int s,int x,int y){
+    for (int i = 0; i < N ; i++)
+    {
+        for (int j = 0; j < M ; j++)
+        {
+            if ( sqrt(pow(i-x,2)+(pow(j-y,2))) <= s - 1 )
+            {
+                B[i][j] = 1 ;
+                
+            }
+            
+        }
+    }
+}
